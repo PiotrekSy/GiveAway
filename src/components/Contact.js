20bc5e008f878b7f22523b4c2f571c74d0d9d5fd
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-07-04 02:35:24 +0200

    stylowanie skonczone

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,52 @@
 import React from "react";
 
 const Contact = () => {
-    return (
-        <div className="contact">
-            <div className="leftSide"/>
-            <div className="rightSide">
+    return (<div className="contact">
+        <div className="leftSide"/>
+        <div className="rightSide">
+            <div className="contactTitle">Skontaktuj się z nami</div>
+            <div className="contactDecoration"/>
 
-                <div className="contactTitle">Skontaktuj się z nami</div>
-                <div className="contactDecoration"/>
-                <form className="form">
-                    <div className="upperLabels">
-                        <label>Wpisz swoje imię
-                            <input type="name" placeholder="Jan"/>
-                        </label>
-                        <label>Wpisz swój email
-                            <input type="email" placeholder="jankowalski@gmail.pl"/>
-                        </label>
+            <form className="form">
+                <div className="upperLabels">
+                    <div className="upperInputWrapper">
+                        <div className="upperLabel">Wpisz swoje imię</div>
+                        <input className="upperInput" type="name" placeholder="Jan"/>
                     </div>
-                    <div className="bottomLabel">
-                        <label>Wpisz swoją wiadomość
-                            <input type="textarea"
-                                   placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ex,
-                               facere laborum nam necessitatibus quas quod recusandae repudiandae sapiente veritatis.
-                               Aliquam consequatur esse facere iste laboriosam praesentium ut vitae. Id!"/>
-                        </label>
-                    </div>
-                    <button type="submit">Wyślij</button>
-                </form>
-
-                <p></p>
-            </div>
-            <div className="footer">
-                <div className="footerCopyright">
-                    <div>
-                        Copyright by Coders Lab
+                    <div className="upperInputWrapper">
+                        <div className="upperLabel">Wpisz swój email</div>
+                        <input className="upperInput" type="email" placeholder="jankowalski@gmail.pl"/>
                     </div>
                 </div>
-                <div className="footerMediaWrapper">
-                    <div className="footerMedia">
-                        <div>FB ICON</div>
-                        <div>INSTAGRAM ICON</div>
+                <div className="bottomLabel">
+                    <div>Wpisz swoją wiadomość</div>
+                    <textarea className="bottomInput"
+                              placeholder="Lorem ipsum dolor sit amet, facere laborum nam necessitatibus quas quod recusandae repudiandae sapiente veritatis. Aliquam consequatur esse facere iste laboriosam praesentium ut vitae. Id! Dolores ex,facere laborum nam necessitatibus quas quod recusandae repudiandae sapiente veritatis. Aliquam consequatur esse facere iste laboriosam praesentium ut vitae. Id!"/>
+                </div>
+                <div className="formButtonWrapper">
+                    <div className="formButton">
+                        <button className="formButtonRange" type="submit">
+                            <p className="formButtonText">Wyślij</p>
+                        </button>
                     </div>
+                </div>
+            </form>
+
+        </div>
+        <div className="footer">
+            <div className="footerCopyright">
+                <div>
+                    Copyright by Coders Lab
+                </div>
+            </div>
+            <div className="footerMediaWrapper">
+                <div className="footerMedia">
+                    <div className="facebookIcon"/>
+                    <div className="instagramIcon"/>
                 </div>
             </div>
         </div>
-    )
+    </div>)
 }
 
 export default Contact
