c2483151654db8297a29e90c30e503a6f08c4f0e
Piotr S <84278062+PiotrSyjud@users.noreply.github.com>
2022-07-07 21:46:16 +0200

    w trakcie validacji

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,7 @@ import AuthSection from "./AuthSection";
 import RegisterNavbar from "./RegisterNavbar";
 import {Link} from "react-router-dom";
 import "firebase/auth"
-import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import {createUserWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
 import db, {auth} from "./firebase"
 import {useEffect} from "react";
 import {collection, onSnapshot, addDoc} from "@firebase/firestore";
@@ -14,10 +14,9 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [error, setError] = useState('');
+    const [passwordError, setPasswordError] = useState("")
     const [user, setUser] = useState({});
-    const [registered, setRegistered] = useState("");
-    const [isUsingForm, setIsUsingForm] = useState(false);
+    const [passwordConstantError, setPasswordConstantError] = useState("")
 
     useEffect(() => {
         onSnapshot(collection(db, "users"), (snapshot) => {
@@ -28,121 +27,121 @@ const Register = () => {
     //funkcja umieszczająca usera w database;
     const addNewUser = async () => {
         const userCollection = collection(db, "users");
-        const regData = {email: email, password: password, userType: "normal"}
-        await addDoc(userCollection, regData)
+        const payload = {email: email, password: password, userType: "normal"}
+        await addDoc(userCollection, payload)
     }
-
+    //rejestracja nowego konta po warunkiem tych samych haseł
     const register = async (e) => {
         e.preventDefault()
         if (password !== passwordConfirm) {
-            return setError('Passwords do not match!')
+            setPasswordError('Hasła nie są identyczne!')
         }
         try {
-            setError("")
             await createUserWithEmailAndPassword(auth, email, password)
             await addNewUser()
             onAuthStateChanged(auth, (currentUser) => setUser(currentUser))
-            setError("Zarejestrowano!")
-
+            setPasswordError("Zarejestrowano, przejdź do logowania :)")
         } catch {
-            setError("Failed to create an account!")
+            setPasswordError("Failed to create an account!")
+        }
+    }
+
+    //VALKIDACJE
+    // validacja emaila:
+    const [emailError, setEmailError] = useState("");
+
+    function isValidEmail(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    const emailCheck = () => {
+        if (!isValidEmail(email)) {
+            setEmailError('Podany email jest nieprawidłowy');
+        } else {
+            setEmailError("");
+        }
+    }
+    //walidacja haseł:
+
+    const passwordConstantCheck = () => {
+        if (password.length > 0 && password.length <= 6) {
+            setPasswordConstantError('Hasło ma mieć przynajmniej 6 znaków');
+        } else {
+            setPasswordConstantError("");
         }
     }
 
 
-    return (<div className="registrationForm">
-        <AuthSection className="authSection"/>
-        <RegisterNavbar className="navbar"/>
-        <div className="regForm">
-            <div className="regTitle">Załóż konto</div>
-            <div className="regDecoration"></div>
-            <form className="greyArea" onSubmit={register}>
-                {/*labelki*/}
-                <label className="regLabel" htmlFor="email">Email:</label>
-                <input className="regInput" type="email"
-                       id="email"
-                       onChange={e => setEmail(e.target.value)}>
-                </input>
-                {/*<div className="errorMessage"><b>{emailError}</b></div>*/}
+    return (
+        <div className="registrationForm">
+            <AuthSection className="authSection"/>
+            <RegisterNavbar className="navbar"/>
+            <div className="regForm">
+                <div className="regTitle">Załóż konto</div>
+                <div className="regDecoration"></div>
+                <form className="greyArea" onSubmit={register}>
 
-                <label className="regLabel" htmlFor="password">Hasło:</label>
-                <input className="regInput" type="password"
-                       id="password"
-                       onChange={e => setPassword(e.target.value)}>
+                    {/*labelki*/}
 
-                </input>
-                {/*<div className="errorMessage"><b>{passwordError}</b></div>*/}
+                    <label className="regLabel" htmlFor="email">Email:</label>
+                    <input className="regInput" type="email"
+                           id="email"
+                           onChange={e => {
+                               setEmail(e.target.value)
 
-                <label className="regLabel">Powtórz hasło:</label>
-                <input className="regInput" type="password"
-                       id="passwordConfirm"
-                       onChange={e => setPasswordConfirm(e.target.value)}
-                ></input>
-                {/*<div className="errorMessage"><b>{passwordConfirmError}</b></div>*/}
+                           }}>
+                    </input>
+                    {emailError !== "" && <div>{emailError}</div>}
 
-                {/*buttony*/}
-                <div className="regButtons">
-                    <div className="regButton">
-                        <button className="regButtonRange"
-                                type="button"
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    console.log("Loguję się !!!")
-                                }}>
-                            <Link to='/Login'
-                                  className="regLoginLink">
-                                <p className="regButtonText">Zaloguj się</p>
-                            </Link>
-                        </button>
+                    <label className="regLabel" htmlFor="password">Hasło:</label>
+                    <input className="regInput" type="password"
+                           id="password"
+                           onChange={e => {
+                               passwordConstantCheck();
+                               setPassword(e.target.value)
+                           }}>
+                    </input>
+                    {passwordConstantError !== "" && <div>{passwordConstantError}</div>}
+                    <label className="regLabel">Powtórz hasło:</label>
+                    <input className="regInput" type="password"
+                           id="passwordConfirm"
+                           onChange={e => setPasswordConfirm(e.target.value)}>
+                    </input>
+
+                    {passwordError !== "" && <p>{passwordError}</p>}
+
+                    {/*buttony*/}
+                    <div className="regButtons">
+                        <div className="regButton">
+                            <button className="regButtonRange"
+                                    type="button"
+                                    onClick={(e) => {
+                                        e.preventDefault()
+                                        console.log("Loguję się !!!")
+                                    }}>
+                                <Link to='/Login'
+                                      className="regLoginLink">
+                                    <p className="regButtonText">Zaloguj się</p>
+                                </Link>
+                            </button>
+                        </div>
+                        <div className="regButton">
+                            <button className="regButtonRange"
+                                    type="submit"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        emailCheck();
+                                        passwordConstantCheck();
+                                        register();
+                                        console.log("Rejestruję się !!!");
+                                    }}>
+                                <div className="regButtonText">Załóż konto</div>
+                            </button>
+                        </div>
                     </div>
-                    <div className="regButton">
-                        <button className="regButtonRange"
-                                type="submit"
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    console.log("Rejestruję się !!!");
-                                }}>
-                            <div className="regButtonText">Załóż konto</div>
-                        </button>
-                    </div>
-                </div>
-
-            </form>
-
-
-        </div>
-    </div>)
+                </form>
+            </div>
+        </div>)
 }
-
-
-// <form onSubmit={register} className="registrationForm">
-//         <>
-//             <div>
-//                 <h1 className="pageTitle">Register Now! </h1>
-//             </div>
-//             <div className="registrationInputWrapper">
-//                 <label className="description" htmlFor="email"></label>
-//                 <input className="inputVisuals" type="email"
-//                        id="email"
-//                        onChange={e => setEmail(e.target.value)}/>
-//             </div>
-
-//             <div className="registrationInputWrapper">
-//                 <label className="description" htmlFor="password"></label>
-//                 <input className="inputVisuals" type="password"
-//                        id="password"
-//                        onChange={e => setPassword(e.target.value)}/>
-//             </div>
-
-//             <div className="registrationInputWrapper">
-//                 <label className="description" htmlFor="passwordConfirm"></label>
-//                 <input className="inputVisuals" type="password"
-//                        id="passwordConfirm"
-//                        onChange={e => setPasswordConfirm(e.target.value)}/>
-//             </div>
-
-//        </>}
-// </form>
-
 
 export default Register
